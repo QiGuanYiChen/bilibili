8cb5a5db684ffcefdec291d343df747c82835af2
Daybreak <450552846@qq.com>
2017-03-08 22:46:29 +0800

    router

--- public/js/index.js
+++ public/js/index.js
@@ -3,19 +3,21 @@ let app = angular.module("app", ["ngRoute", "angularCSS"]);
 app.config(["$routeProvider", function ($routeProvider) {
     // when(a,b)  a. 锚点路径  b. object
     $routeProvider
-        .when("#home", {
+        .when("/home", {
+            template: "<h1>这是home页面</h1>"
             // templateUrl:
             // css:
             // controller:
         })
-        .when("#page", {
+        .when("/channel", {
+            template: "<h1>channel</h1>"
             // html 路径
             // templateUrl:
             // css:
             // controller:
         })
         // 默认跳转
-        .otherwise("#home");
+        .otherwise("/home");
 }]);
 
 // 解决手机端点击 300毫秒 延迟
